4ef9163e5cee597ff4a168ee2799f48bcd6a0b02
Isabella Reis <isabellanery15@hotmail.com>
2023-11-29 18:33:14 -0300

    feat: adicionando formulario com validacao pelo zod

--- ecommuna-ecommerce/components/modals/store-modal.tsx
+++ ecommuna-ecommerce/components/modals/store-modal.tsx
@@ -1,10 +1,40 @@
 "use client";
 
-import { useStoreModal } from "@/hooks/use-store-modal";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Modal } from "@/components/ui/modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  // pelo menos 1 char é necessário nos nomes das lojas (stores)
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // todo: criar loja
+    console.log(values);
+  };
 
   return (
     <Modal
@@ -13,7 +43,31 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Futuro formulário para criar loja
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nome do E-Commerce" {...field} />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant={"outline"} onClick={storeModal.onClose}>Cancelar</Button>
+                <Button type="submit">Continuar</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
