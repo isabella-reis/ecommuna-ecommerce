20f829bd801e8d20b3a877363838e638f8cd83f4
Isabella Reis <isabellanery15@hotmail.com>
2023-11-29 15:31:03 -0300

    bugfix: errei o import

--- ecommuna-ecommerce/app/(root)/page.tsx
+++ ecommuna-ecommerce/app/(root)/page.tsx
@@ -1,11 +1,13 @@
+
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
-    <div className="p-4">
+    <div className="p-4 bg-red-300">
       <UserButton afterSignOutUrl="/"/>
-      <Button >aaa</Button>
+      <Button variant={"outline"}><p>aaaaaaaaaaaaaaaaaaaaaaaaa</p></Button>
+      
     </div>
   );
 }
