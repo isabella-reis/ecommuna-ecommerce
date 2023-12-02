bca0f4918130a08bd377631ab940b3dafe2c6093
Isabella Reis <isabellanery15@hotmail.com>
2023-12-02 20:36:05 -0300

    feat: add api troca de nome das lojas

--- ecommuna-ecommerce/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ ecommuna-ecommerce/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,9 +1,12 @@
 "use client";
 import * as z from "zod";
+import axios from "axios";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { Trash } from "lucide-react";
 import { Store } from "@prisma/client";
 import { useForm } from "react-hook-form";
+import { useParams, useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
@@ -30,6 +33,8 @@ const formSchema = z.object({
 type SettingsFormValues = z.infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<ISettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -39,56 +44,66 @@ export const SettingsForm: React.FC<ISettingsFormProps> = ({ initialData }) => {
   });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log("DATAVALUES", data);
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh()
+      toast.success("Loja Atualizada.")
+    } catch (error) {
+      toast.error("Algo deu errado. :(");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+    return (
+      <>
+        <div className="flex items-center justify-between">
+          <Heading
+            title="Configurações"
+            description="Gerenciar preferências da loja"
+          />
+          <Button
+            disabled={loading}
+            variant={"destructive"}
+            size={"icon"}
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        </div>
+        <Separator />
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 w-full"
+          >
+            <div className="grid grid-cols-3 gap-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Nome da Loja"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button disabled={loading} className="ml-auto" typeof="submit">
+              Salvar Alterações
+            </Button>
+          </form>
+        </Form>
+      </>
+    );
   };
 
-  return (
-    <>
-      <div className="flex items-center justify-between">
-        <Heading
-          title="Configurações"
-          description="Gerenciar preferências da loja"
-        />
-        <Button
-          disabled={loading}
-          variant={"destructive"}
-          size={"icon"}
-          onClick={() => setOpen(true)}
-        >
-          <Trash className="h-4 w-4" />
-        </Button>
-      </div>
-      <Separator />
-
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 w-full"
-        >
-          <div className="grid grid-cols-3 gap-8">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="Nome da Loja"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button disabled={loading} className="ml-auto" typeof="submit">
-            Salvar Alterações
-          </Button>
-        </form>
-      </Form>
-    </>
-  );
-};
