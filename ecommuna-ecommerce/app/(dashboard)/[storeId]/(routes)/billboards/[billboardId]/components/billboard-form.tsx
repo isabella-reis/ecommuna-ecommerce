48dc8149590784708d27db8dd24ec6a17d7b2db1
Isabella Reis <isabellanery15@hotmail.com>
2023-12-04 11:21:32 -0300

    feat: add tabela

--- ecommuna-ecommerce/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ ecommuna-ecommerce/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -22,13 +22,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
-import { useOrigin } from "@/hooks/use-origin";
 import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
   label: z.string().min(1),
-  imageUrl: z.string().min(1)
+  imageUrl: z.string().min(1),
 });
 
 type BillboardFormValues = z.infer<typeof formSchema>;
@@ -37,133 +35,142 @@ interface IBillboardFormProps {
   initialData: Billboard | null;
 }
 
-export const BillboardForm: React.FC<IBillboardFormProps> = ({ initialData }) => {
+export const BillboardForm: React.FC<IBillboardFormProps> = ({
+  initialData,
+}) => {
   const params = useParams();
   const router = useRouter();
-  const origin = useOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const title = initialData ? "Editar billboard" : "Criar billboard";
-  const description = initialData ? "Editar um billboard" : "Adicionar um novo billboard";
-  const toastMessage = initialData ? "Billboard atualizado." : "Billboard criado.";
+  const description = initialData
+    ? "Editar um billboard"
+    : "Adicionar um novo billboard";
+  const toastMessage = initialData
+    ? "Billboard atualizado."
+    : "Billboard criado.";
   const action = initialData ? "Salvar alterações" : "Criar";
 
   const form = useForm<BillboardFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
-      label: '',
-      imageUrl: ''
-    }
+      label: "",
+      imageUrl: "",
+    },
   });
 
   const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true);
-      if(initialData){
-        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
-      }else{
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/billboards/${params.billboardId},`,
+          data
+        );
+      } else {
         await axios.post(`/api/${params.storeId}/billboards`, data);
       }
-      router.refresh()
-      router.push(`/${params.storeId}/billboards`)
-      toast.success(toastMessage)
+      router.refresh();
+      router.push(`/${params.storeId}/billboards`);
+      toast.success(toastMessage);
     } catch (error) {
       toast.error("Algo deu errado. :(");
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   const onDelete = async () => {
     try {
-      setLoading(true)
-      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
+      setLoading(true);
+      await axios.delete(
+        `/api/${params.storeId}/billboards/${params.billboardId}`
+      );
       router.refresh();
-      router.push("/")
+      router.push("/");
       toast.success("Billboard deletada.");
-    } catch(error){
-      toast.error("Certifique-se de remover todas as categorias que utilizam esse billboard primeiro.")
+    } catch (error) {
+      toast.error(
+        "Certifique-se de remover todas as categorias que utilizam esse billboard primeiro."
+      );
     } finally {
-      setLoading(false)
-      setOpen(false)
+      setLoading(false);
+      setOpen(false);
     }
-  }
+  };
 
-    return (
-      <>
-        <AlertModal
+  return (
+    <>
+      <AlertModal
         isOpen={open}
         onClose={() => setOpen(false)}
         onConfirm={onDelete}
         loading={loading}
-        />
-        <div className="flex items-center justify-between">
-          <Heading
-            title={title}
-            description={description}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant={"destructive"}
+            size={"icon"}
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Imagem de fundo</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-          {initialData &&(
-            <Button
-              disabled={loading}
-              variant={"destructive"}
-              size={"icon"}
-              onClick={() => setOpen(true)}
-            >
-              <Trash className="h-4 w-4" />
-            </Button>
-          )}
-        </div>
-        <Separator />
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-8 w-full">
+          <div className="grid grid-cols-3 gap-8">
             <FormField
-                control={form.control}
-                name="imageUrl"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Imagem de fundo</FormLabel>
-                    <FormControl>
-                      <ImageUpload 
-                        value={field.value ? [field.value] : []}
-                        disabled = {loading}
-                        onChange = {(url) => field.onChange(url)}
-                        onRemove = {() => field.onChange("")}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            <div className="grid grid-cols-3 gap-8">
-              <FormField
-                control={form.control}
-                name="label"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome</FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={loading}
-                        placeholder="Nome do Billboard"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <Button disabled={loading} className="ml-auto" typeof="submit">
-              {action}
-            </Button>
-          </form>
-        </Form>
-        <Separator />
-      </>
-    );
-  };
-
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Nome do Billboard"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" typeof="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
--- ecommuna-ecommerce/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommuna-ecommerce/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,29 +3,36 @@
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
+import { columns } from "./columns";
+import { BillboardColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
 
 
-export const BillboardClient = () => {
+interface IBillboardClientProps {
+    data: BillboardColumn[]
+}
+
+export const BillboardClient = ({data}: IBillboardClientProps) => {
     const router = useRouter();
     const params = useParams();
-
 
     return(
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title="Billboards (0)"
-                    description="Gerencie outdoors para sua loja."
+                    title={`Billboards (${data.length})`}
+                    description="Gerencie billboards para sua loja."
                 />
-                <Button onClick={() => router.push(`${params.storeId}/billboards/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4"/>
                     Adicione um novo
                 </Button>
             </div>
             <Separator/>
+            <DataTable columns={columns} data={data}/>
         </>
     )
 }
