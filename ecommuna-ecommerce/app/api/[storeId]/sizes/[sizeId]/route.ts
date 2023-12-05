4c86061bfa54f278e2f4e2583735a64b96da802a
Isabella Reis <isabellanery15@hotmail.com>
2023-12-05 11:44:46 -0300

    feat: add tamanhos

---
+++ ecommuna-ecommerce/app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -0,0 +1,118 @@
+import SizePage from "@/app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page";
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+    _req: Request,
+    { params }: { params: { sizeId: string } }
+  ) {
+    try {
+      if (!params.sizeId) {
+        return new NextResponse("'Tamanho' é obrigatório.", { status: 400 });
+      }
+
+      const size = await prismadb.size.findUnique({
+        where: {
+          id: params.sizeId,
+        },
+      });
+
+      return NextResponse.json(size);
+    } catch (error) {
+      console.log("SIZE_GET", error);
+      return new NextResponse("Erro interno", { status: 500 });
+    }
+};
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string, sizeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name, value } = body;
+
+    if (!userId) {
+      return new NextResponse("Não autenticado", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("'Nome' é obrigatório.", { status: 400 });
+    }
+
+    if (!value) {
+        return new NextResponse("'Valor' é obrigatório.", { status: 400 });
+      }
+
+    if (!params.sizeId) {
+      return new NextResponse("'Tamanho' é obrigatório.", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    });
+
+    if (!storeByUserId){
+        return new NextResponse("Não autorizado", { status: 403 });
+    }
+
+    const size = await prismadb.size.updateMany({
+      where: {
+        id: params.sizeId,
+      },
+      data: {
+        name,
+        value
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("SIZE_PATCH", error);
+    return new NextResponse("Erro interno", { status: 500 });
+  }
+};
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string, sizeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Não autenticado", { status: 401 });
+    }
+
+    if (!params.sizeId) {
+      return new NextResponse("'Tamanho' é obrigatório.", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    });
+
+    if (!storeByUserId){
+        return new NextResponse("Não autorizado", { status: 403 });
+    }
+
+    const size = await prismadb.size.deleteMany({
+      where: {
+        id: params.sizeId,
+      },
+    });
+
+    return NextResponse.json(SizePage);
+  } catch (error) {
+    console.log("SIZE_DELETE", error);
+    return new NextResponse("Erro interno", { status: 500 });
+  }
+};
