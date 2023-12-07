3bcea6cd0ee579f0a03a383965b55d033d0e1f13
Isabella Reis <isabellanery15@hotmail.com>
2023-12-07 18:58:44 -0300

    bugfix.

--- ecommuna-ecommerce/app/(auth)/layout.tsx
+++ ecommuna-ecommerce/app/(auth)/layout.tsx
@@ -1,5 +1,11 @@
-export default function AuthLayout({ children }: React.ReactNode) {
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <div className="flex items-center justify-center h-full">{children}</div>
+    <div className="flex items-center justify-center h-full w-full">
+      {children}
+    </div>
   );
-}
+};
